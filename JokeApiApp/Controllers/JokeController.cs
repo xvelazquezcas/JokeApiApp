8afe939810af7272997541b2a960a657888e0b11
velazquezxana597@gmail.com <velazquezxana597@gmail.com>
2024-10-28 14:42:16 -0700

    Agregar archivos de proyecto.

---
+++ JokeApiApp/Controllers/JokeController.cs
@@ -0,0 +1,21 @@
+﻿using JokeApiApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JokeApiApp.Controllers
+{
+    public class JokeController : Controller
+    {
+        private readonly JokeService jokeService = new JokeService();
+        public async Task<ActionResult> Index()
+        {
+            var joke = await jokeService.GetRandomJokeAsync();
+            return View(joke);
+        }
+
+    }
+}
---
+++ JokeApiApp/Models/JokeService.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace JokeApiApp.Models
+{
+    public class JokeService
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        public async Task<Joke> GetRandomJokeAsync()
+        {
+            var response = await httpClient.GetAsync("https://v2.jokeapi.dev/joke/Programming,Spooky,Christmas");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Joke>(content);
+        }
+    }
+
+    public class Joke
+    {
+        public string Setup { get; set; }
+        public string Delivery { get; set; }
+        public string JokeText{ get; set; }
+
+        public bool IsSetup => Setup != null;
+    }
+}
